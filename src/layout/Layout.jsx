690d9151a65360967ddb04e7021461c0a75a7e8f
Nicolas Ollier <nicolas@lepetitmartin.com>
2023-11-15 18:24:34 +0100

    install sass

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from "./Header.jsx";
-import Footer from "./Footer.jsx";
+import Header from "./header/Header.jsx";
+import Footer from "./footer/Footer.jsx";
 
 const Layout = () => {
   return (
---
+++ src/layout/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Footer.module.scss";
+import LogoKasa from "../../assets/logos/LogoKasa";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <LogoKasa className={styles.footer__logo} />
+      <p>© 2023 Kasa. All rights reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
