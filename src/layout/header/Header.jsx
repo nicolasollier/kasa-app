9dcac05280e3eac56eea6d6bd5ec441680c40655
Nicolas Ollier <nicolas@lepetitmartin.com>
2023-11-16 10:48:09 +0100

    add logos on homepage

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import LogoKasa from "../../assets/logos/LogoKasa";
+import styles from "./Header.module.sass";
 
 const Header = () => {
   return (
-    <nav>
+    <nav className={styles.nav}>
+      <LogoKasa className={styles.nav__logo} fillColor="#FF6060"/>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/">Acceuil</Link>
         </li>
         <li>
-          <Link to="/accomodations">Accomodations</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
+          <Link to="/about">A propos</Link>
         </li>
       </ul>
     </nav>
