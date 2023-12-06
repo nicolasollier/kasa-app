830fa72944caa9bb15015ce7b02d90f9574f924c
Nicolas Ollier <nicolas@lepetitmartin.com>
2023-12-06 18:11:50 +0100

    fix badges

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,16 +1,17 @@
 import styles from "./Avatar.module.sass";
+
 const Avatar = ({ host }) => {
   const { picture, name } = host;
+
   const parsedName = () => {
     const splitName = name.split(" ");
-    return (
-      <>
-        <span>{splitName[0]}</span>
-        <br />
-        <span>{splitName[1]}</span>
-      </>
-    );
+    return splitName.map((word, index) => (
+      <span key={index} className={styles.namePart}>
+        {word}
+      </span>
+    ));
   };
+
   return (
     <div className={styles.avatar}>
       <p className={styles.avatar__name}>{parsedName()}</p>
